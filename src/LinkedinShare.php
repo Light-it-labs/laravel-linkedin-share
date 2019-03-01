4ba7385ba7c35438234383cba1bba8ff98e79cb8
Alan <alanbrande@gmail.com>
2019-03-01 14:53:13 +0000

    Apply fixes from StyleCI

--- src/LinkedinShare.php
+++ src/LinkedinShare.php
@@ -15,7 +15,7 @@ class LinkedinShare
     {
         $i = func_num_args();
         if ($i != 3) {
-            throw new LinkedinShareException("Invalid arguments. Use REDIRECT_URL, CLIENT_ID and CLIENT_SECRET" . $i);
+            throw new LinkedinShareException('Invalid arguments. Use REDIRECT_URL, CLIENT_ID and CLIENT_SECRET'.$i);
         }
 
         $this->redirect_uri = func_get_arg(0);
@@ -23,22 +23,22 @@ class LinkedinShare
         $this->client_secret = func_get_arg(2);
     }
 
-
     public function getAccessToken($code)
     {
         $client = new Client();
         $response = $client->request('POST', 'https://www.linkedin.com/oauth/v2/accessToken', [
             'form_params' => [
-                'grant_type' => 'authorization_code',
-                'code' => $code,
-                'redirect_uri' => $this->redirect_uri,
-                'client_id' => $this->client_id,
-                'client_secret' => $this->client_secret
-            ]
+                'grant_type'    => 'authorization_code',
+                'code'          => $code,
+                'redirect_uri'  => $this->redirect_uri,
+                'client_id'     => $this->client_id,
+                'client_secret' => $this->client_secret,
+            ],
         ]);
 
         $object = json_decode($response->getBody()->getContents(), true);
         $access_token = $object['access_token'];
+
         return $access_token;
     }
 
@@ -47,11 +47,12 @@ class LinkedinShare
         $client = new Client();
         $response = $client->request('GET', 'https://api.linkedin.com/v2/me', [
             'headers' => [
-                'Authorization' => 'Bearer ' . $access_token,
-                'Connection' => 'Keep-Alive',
+                'Authorization' => 'Bearer '.$access_token,
+                'Connection'    => 'Keep-Alive',
             ],
         ]);
         $object = json_decode($response->getBody()->getContents(), true);
+
         return $object;
     }
 
@@ -61,26 +62,27 @@ class LinkedinShare
 
         $response = $client->request('POST', 'https://api.linkedin.com/v2/assets?action=registerUpload', [
             'headers' => [
-                'Authorization' => 'Bearer ' . $access_token,
-                'Connection' => 'Keep-Alive',
-                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer '.$access_token,
+                'Connection'    => 'Keep-Alive',
+                'Content-Type'  => 'application/json',
             ],
             'json' => [
-                "registerUploadRequest" => [
-                    "recipes" => [
-                        "urn:li:digitalmediaRecipe:feedshare-image"
+                'registerUploadRequest' => [
+                    'recipes' => [
+                        'urn:li:digitalmediaRecipe:feedshare-image',
                     ],
-                    "owner" => "urn:li:person:" . $personURN,
-                    "serviceRelationships" => [
+                    'owner'                => 'urn:li:person:'.$personURN,
+                    'serviceRelationships' => [
                         [
-                            "relationshipType" => "OWNER",
-                            "identifier" => "urn:li:userGeneratedContent"
-                        ]
-                    ]
-                ]
-            ]
+                            'relationshipType' => 'OWNER',
+                            'identifier'       => 'urn:li:userGeneratedContent',
+                        ],
+                    ],
+                ],
+            ],
         ]);
         $object = json_decode($response->getBody()->getContents(), true);
+
         return $object;
     }
 
@@ -89,7 +91,7 @@ class LinkedinShare
         $client = new Client();
         $client->request('PUT', $url, [
                 'headers' => [
-                    'Authorization' => 'Bearer ' . $access_token
+                    'Authorization' => 'Bearer '.$access_token,
                 ],
                 'body' => fopen($image, 'r'),
 
@@ -107,39 +109,37 @@ class LinkedinShare
         $uploadUrl = $uploadObject['value']['uploadMechanism']['com.linkedin.digitalmedia.uploading.MediaUploadHttpRequest']['uploadUrl'];
         $this->uploadImage($uploadUrl, $access_token, $image);
 
-
         $client->request('POST', 'https://api.linkedin.com/v2/ugcPosts', [
             'headers' => [
-                'Authorization' => 'Bearer ' . $access_token,
-                'Connection' => 'Keep-Alive',
-                'Content-Type' => 'application/json',
-                'X-Restli-Protocol-Version' => '2.0.0'
+                'Authorization'             => 'Bearer '.$access_token,
+                'Connection'                => 'Keep-Alive',
+                'Content-Type'              => 'application/json',
+                'X-Restli-Protocol-Version' => '2.0.0',
             ],
             'json' => [
-                "author" => "urn:li:person:" . $personURN,
-                "lifecycleState" => "PUBLISHED",
-                "specificContent" => [
-                    "com.linkedin.ugc.ShareContent" => [
-                        "shareCommentary" => [
-                            "text" => $text
+                'author'          => 'urn:li:person:'.$personURN,
+                'lifecycleState'  => 'PUBLISHED',
+                'specificContent' => [
+                    'com.linkedin.ugc.ShareContent' => [
+                        'shareCommentary' => [
+                            'text' => $text,
                         ],
-                        "shareMediaCategory" => "IMAGE",
-                        "media" => [
+                        'shareMediaCategory' => 'IMAGE',
+                        'media'              => [
                             [
-                                "status" => "READY",
+                                'status' => 'READY',
                                 //"originalUrl" => "https://linkedin.com/",
-                                "media" => $asset,
+                                'media' => $asset,
 
-                            ]
-                        ]
-                    ]
+                            ],
+                        ],
+                    ],
                 ],
-                "visibility" => [
-                    "com.linkedin.ugc.MemberNetworkVisibility" => "PUBLIC"
-                ]
-            ]
+                'visibility' => [
+                    'com.linkedin.ugc.MemberNetworkVisibility' => 'PUBLIC',
+                ],
+            ],
         ]);
-
     }
 
     public function shareArticle($code, $url, $text)
@@ -148,38 +148,36 @@ class LinkedinShare
         $access_token = $this->getAccessToken($code);
         $personURN = $this->getProfile($access_token)['id'];
 
-
         $client->request('POST', 'https://api.linkedin.com/v2/ugcPosts', [
             'headers' => [
-                'Authorization' => 'Bearer ' . $access_token,
-                'Connection' => 'Keep-Alive',
-                'Content-Type' => 'application/json',
-                'X-Restli-Protocol-Version' => '2.0.0'
+                'Authorization'             => 'Bearer '.$access_token,
+                'Connection'                => 'Keep-Alive',
+                'Content-Type'              => 'application/json',
+                'X-Restli-Protocol-Version' => '2.0.0',
             ],
             'json' => [
-                "author" => "urn:li:person:" . $personURN,
-                "lifecycleState" => "PUBLISHED",
-                "specificContent" => [
-                    "com.linkedin.ugc.ShareContent" => [
-                        "shareCommentary" => [
-                            "text" => $text
+                'author'          => 'urn:li:person:'.$personURN,
+                'lifecycleState'  => 'PUBLISHED',
+                'specificContent' => [
+                    'com.linkedin.ugc.ShareContent' => [
+                        'shareCommentary' => [
+                            'text' => $text,
                         ],
-                        "shareMediaCategory" => "ARTICLE",
-                        "media" => [
+                        'shareMediaCategory' => 'ARTICLE',
+                        'media'              => [
                             [
-                                "status" => "READY",
-                                "originalUrl" => $url
+                                'status'      => 'READY',
+                                'originalUrl' => $url,
 
-                            ]
-                        ]
-                    ]
+                            ],
+                        ],
+                    ],
                 ],
-                "visibility" => [
-                    "com.linkedin.ugc.MemberNetworkVisibility" => "PUBLIC"
-                ]
-            ]
+                'visibility' => [
+                    'com.linkedin.ugc.MemberNetworkVisibility' => 'PUBLIC',
+                ],
+            ],
         ]);
-
     }
 
     public function shareNone($code, $text)
@@ -188,37 +186,31 @@ class LinkedinShare
         $access_token = $this->getAccessToken($code);
         $personURN = $this->getProfile($access_token)['id'];
 
-
         $client->request('POST', 'https://api.linkedin.com/v2/ugcPosts', [
             'headers' => [
-                'Authorization' => 'Bearer ' . $access_token,
-                'Connection' => 'Keep-Alive',
-                'Content-Type' => 'application/json',
-                'X-Restli-Protocol-Version' => '2.0.0'
+                'Authorization'             => 'Bearer '.$access_token,
+                'Connection'                => 'Keep-Alive',
+                'Content-Type'              => 'application/json',
+                'X-Restli-Protocol-Version' => '2.0.0',
             ],
             'json' => [
-                "author" => "urn:li:person:" . $personURN,
-                "lifecycleState" => "PUBLISHED",
-                "specificContent" => [
-                    "com.linkedin.ugc.ShareContent" => [
-                        "shareCommentary" => [
-                            "text" => $text
+                'author'          => 'urn:li:person:'.$personURN,
+                'lifecycleState'  => 'PUBLISHED',
+                'specificContent' => [
+                    'com.linkedin.ugc.ShareContent' => [
+                        'shareCommentary' => [
+                            'text' => $text,
                         ],
-                        "shareMediaCategory" => "NONE",
-                    ]
+                        'shareMediaCategory' => 'NONE',
+                    ],
                 ],
-                "visibility" => [
-                    "com.linkedin.ugc.MemberNetworkVisibility" => "PUBLIC"
-                ]
-            ]
+                'visibility' => [
+                    'com.linkedin.ugc.MemberNetworkVisibility' => 'PUBLIC',
+                ],
+            ],
         ]);
-
     }
-
 }
-
-
-
 
 class LinkedinShareException extends Exception
 {
